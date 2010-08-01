a04434e3228e88da08b01d5450740ba2c2102034
Radim Zatopek <radim.zatopek@gmail.com>
2010-08-01 19:17:39 +0200

    Cista rails aplikace

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_depot_session',
+  :secret      => '83835062870468c3e977010584e1dd6e794529ac0818082bb2f011d78eb15897333fad4d45366c55601074c0db37f836a3157e5940f0043d62ec22b88c6f9bef'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
